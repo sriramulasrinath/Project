57a834efa18355e2fb999dcededf4710d90e8fed
srinath sriramula <sriramulasrinath59@gmail.com>
2024-09-22 19:59:07 +0530

    commiting changes

--- terraform-ec2/ansible.sh
+++ terraform-ec2/ansible.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-
-
-https://github.com/sriramulasrinath/Project.git
-
+env > /tmp/env_info.log
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+git clone https://github.com/sriramulasrinath/Project.git &>>$LOGFILE
 dnf install ansible -y 
+
+
+
+
+# Clone the repository and log output
+if git clone https://github.com/sriramulasrinath/Project.git >> $LOGFILE 2>&1; then
+    echo "Git clone succeeded." >> $LOGFILE
+else
+    echo "Git clone failed." >> $LOGFILE
+fi
